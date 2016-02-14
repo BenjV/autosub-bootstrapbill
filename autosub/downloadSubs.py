c1c353a34df80bf700e190d86804586589847366
Ben Vergeer <Benj.vergeer@gmail.com>
2016-02-14 14:23:19 +0100

    corrected wrong var name

--- autosub/downloadSubs.py
+++ autosub/downloadSubs.py
@@ -192,7 +192,7 @@ def DownloadSub(Wanted,SubList):
 
     notify.notify(Wanted['downlang'], destsrt, Wanted["originalFileLocationOnDisk"], Sub['website'])
     if autosub.POSTPROCESSCMD:
-        postprocesscmdconstructed = autosub.POSTPROCESSCMD + ' "' + Wanted["dstFileOnDisk"] + '" "' + Wanted["originalFileLocationOnDisk"] + '" "' + Wanted["downlang"] + '" "' + Wanted["season"] + '" "' + Wanted["episode"] + '" '
+        postprocesscmdconstructed = autosub.POSTPROCESSCMD + ' "' + destsrt + '" "' + Wanted["originalFileLocationOnDisk"] + '" "' + Wanted["downlang"] + '" "' + Wanted["season"] + '" "' + Wanted["episode"] + '" '
         log.debug("downloadSubs: Postprocess: running %s" % postprocesscmdconstructed)
         log.info("downloadSubs: Running PostProcess")
         postprocessoutput, postprocesserr = autosub.Helpers.RunCmd(postprocesscmdconstructed)
