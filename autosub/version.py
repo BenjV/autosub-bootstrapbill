f86336a769302afe8909bddac008d1de02470bf6
Ben Vergeer <Benj.vergeer@gmail.com>
2016-03-30 13:29:21 +0200

    Some layout improvements
    Changed the wanted table for a better fit.
    Added a link to the ImdbId and the Addic7ED Id to open a window to the
    showpage on those websites.
    Added a autorefresh of 30 sec for the main page.

--- autosub/version.py
+++ autosub/version.py
@@ -1,3 +1,3 @@
-autosubversion='Alpha 0.6.23'
+autosubversion='Alpha 0.6.24'
 configversion=3
 dbversion=8
--- interface/media/scripts/autosub-custom.js
+++ interface/media/scripts/autosub-custom.js
@@ -31,7 +31,7 @@ $(document).ready(function () {
 		"bPaginate": true,
 		"aLengthMenu": [ [10, 25, 50, 100, -1], [10, 25, 50, 100, "All"] ],
 		"aaSorting": [ [ 0, "asc"], [ 1, "asc"], [ 2, "asc"] ],
-		"aoColumnDefs": [ { "aDataSort": [ 0, 1, 2 ], "aTargets": [ 0 ] }, { "sType": "euro-date", "aTargets": [ 9 ] }, { "bSortable": false, "aTargets": [ 7, 10 ] } ]
+		"aoColumnDefs": [ { "aDataSort": [ 0, 1, 2 ], "aTargets": [ 0 ] }, { "sType": "euro-date", "aTargets": [ 9 ] }, { "bSortable": false, "aTargets": [ 4, 5, 7, 8, 10 ] } ]
 	});
 
 	$('#downloaded').dataTable({ 
